e89b86a98748dc3ab162f815300465c23c02783b
yasudamasatosi <t.taiga1112@icloud.com>
2024-09-20 09:18:42 +0900

    create function

--- action2d.py
+++ action2d.py
@@ -7,6 +7,10 @@ import math
 pygame.init()
 #ウィンドウの大きさ，名前設定
 screen = pygame.display.set_mode((600, 450))
+
+#画面の左端，右端(画像が32×32なことを考慮)
+leftEdge = 0
+rightEdge = 568
 #背景色の設定
 screen.fill((0, 0, 0))
 pygame.display.set_caption('action2d')
@@ -20,7 +24,7 @@ playerX_Change = 0
 playerY_Change = 0
 
 enemyImg = pygame.image.load('rocket.png')
-enemyX = random.randint(0, 568)
+enemyX = random.randint(leftEdge, rightEdge)
 enemyY = random.randint(50, 100)
 enemyX_Change = 3
 enemyY_Change = 30
@@ -40,6 +44,17 @@ scoreValue = 0
 #プレイヤーを表示する関数
 def displayPlayer(x, y):
     screen.blit(playerImg, (x, y))
+
+def movePlayer(playerX, playerX_Change):
+    #プレイヤーを移動
+    newPos = playerX + playerX_Change
+
+    #画面外にはみ出すのを防止
+    if newPos <= leftEdge:
+        newPos = leftEdge
+    elif newPos > rightEdge:
+        newPos = rightEdge
+    return newPos
 
 def displayEnemy(x, y):
     screen.blit(enemyImg, (x, y))
@@ -86,23 +101,17 @@ while running:
             if event.key == pygame.K_LEFT or pygame.K_RIGHT:
                 playerX_Change = 0
 
-    playerX += playerX_Change
-
-    #画面外にはみ出すのを防止
-    if playerX <= 0:
-        playerX = 0
-    elif playerX > 568:
-        playerX = 568
+        playerX = movePlayer(playerX, playerX_Change)
 
     #敵が一定ラインを越えたらゲームオーバー
     if enemyY > 380:
         break
 
     enemyX += enemyX_Change
-    if enemyX <= 0:
+    if enemyX <= leftEdge:
         enemyX_Change = 3
         enemyY += enemyY_Change
-    elif enemyX >= 568:
+    elif enemyX >= rightEdge:
         enemyX_Change = -3
         enemyY += enemyY_Change
 
@@ -111,7 +120,7 @@ while running:
         bulletY = 400
         bulletState = 'ready'
         scoreValue += 1
-        enemyX = random.randint(0, 568)
+        enemyX = random.randint(leftEdge, rightEdge)
         enemyY = random.randint(50, 100)
 
     if bulletY <= 0:
